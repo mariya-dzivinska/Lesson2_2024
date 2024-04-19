7eeac0470ed038d6bb48d82a586d60e25b104c56
Mariia Dzivinska <mdzivinska@deloitte.com>
2024-04-19 19:59:27 +0300

    merge

---
+++ Lesson2_git/SampleStruct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2_git
+{
+    public struct SampleStruct
+    {
+        public int XPoint { get; set; }
+        public int YPoint { get; set; }
+    }
+}
